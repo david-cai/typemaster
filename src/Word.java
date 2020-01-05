134d1d13450d76fb3ec9a6270c1ef73c699bbb2a
David Cai <57420559+david-cai@users.noreply.github.com>
2020-01-05 00:46:38 -0500

    Added instance variables and constructor for Word class

--- src/Word.java
+++ src/Word.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
+import javax.swing.JTextField;
 
 public class Word {
 
+	private String chosenWord;
+	private JTextField wordField;
+	private int fallSpeed;
+	private int xVal;
+	private int yVal;
+	
+	public Word(String chosenWord, int fallSpeed) {
+		this.chosenWord = chosenWord;
+		this.fallSpeed = fallSpeed;
+		Random r = new Random();
+		xVal = r.nextInt(500);
+		yVal = 50;
+	}
+	
 }
