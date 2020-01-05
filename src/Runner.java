435c23019db299ab6af059dd8493f1a8368abb9c
David Cai <57420559+david-cai@users.noreply.github.com>
2020-01-05 00:37:02 -0500

    Set window size to 600,600 and initialized JFrame

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,24 @@
+import java.io.FileNotFoundException;
 
-public class Runner {
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Runner extends JFrame {
 
+	private GameGUI game;
+	
+	public Runner() throws FileNotFoundException {
+		setSize(600,600);
+
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		game = new GameGUI();
+		setContentPane(game);
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		new Runner();
 	}
 
 }
